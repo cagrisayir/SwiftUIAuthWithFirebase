79317330b778fc5f221bd83cb3e06142c7a82a19
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-20 14:19:11 +0300

    Settings Page

--- SwiftUIAuthWithFirebase/Core/Profile/ProfileView.swift
+++ SwiftUIAuthWithFirebase/Core/Profile/ProfileView.swift
@@ -37,9 +37,30 @@ struct ProfileView: View {
             }
 
             Section("General") {
+                HStack {
+                    SettingsRowView(imageName: "gear", title: "Version", tintColor: Color(.systemGray))
+
+                    Spacer()
+                    Text("1.0.0")
+                        .font(.subheadline)
+                        .foregroundStyle(.gray)
+                }
             }
 
             Section("Account") {
+                Button(action: {
+                    print("Sign Out")
+                }, label: {
+                    SettingsRowView(imageName: "arrow.left.circle.fill", title: "Sign Out", tintColor: Color(.orange))
+
+                })
+
+                Button(action: {
+                    print("Deleted Account")
+                }, label: {
+                    SettingsRowView(imageName: "xmark.circle.fill", title: "Delete Account", tintColor: Color(.red))
+
+                })
             }
         }
     }
