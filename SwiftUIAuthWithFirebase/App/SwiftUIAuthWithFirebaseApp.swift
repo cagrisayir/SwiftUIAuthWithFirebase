10f8a0430a6af34f421833679507a92a2d87a55f
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-20 13:20:01 +0300

    Initial Commit

---
+++ SwiftUIAuthWithFirebase/App/SwiftUIAuthWithFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAuthWithFirebaseApp.swift
+//  SwiftUIAuthWithFirebase
+//
+//  Created by Omer Cagri Sayir on 20.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAuthWithFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIAuthWithFirebase/Core/Authentication/LoginView.swift
@@ -0,0 +1,28 @@
+//
+//  LoginView.swift
+//  SwiftUIAuthWithFirebase
+//
+//  Created by Omer Cagri Sayir on 20.01.2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                // image
+                
+                // form fields
+                
+                // sign in button
+                
+                // sign up button 
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
