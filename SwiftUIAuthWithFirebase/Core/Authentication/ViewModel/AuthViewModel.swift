8ee5b31b3d4d1bcbfd058ec19a7b921152fd251f
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-21 12:03:14 +0300

    created AuthViewModel

--- SwiftUIAuthWithFirebase/Core/Authentication/ViewModel/AuthViewModel.swift
+++ SwiftUIAuthWithFirebase/Core/Authentication/ViewModel/AuthViewModel.swift
@@ -5,4 +5,23 @@
 //  Created by Omer Cagri Sayir on 20.01.2024.
 //
 
+import Firebase
 import Foundation
+
+class AuthViewModel: ObservableObject {
+    @Published var userSession: FirebaseAuth.User? // Firebase Auth user
+    @Published var currentUser: User? // Our user
+
+    init() {
+    }
+
+    func login(withEmail email: String, password: String) async throws {}
+
+    func createUser(withEmail email: String, password: String, fullname: String) async throws {}
+
+    func signOut() {}
+
+    func deleteAccount() {}
+
+    func fetchUser() async {}
+}
